2fb85f02c4271239d7220c099b7947883819b2bc
Logan Ward <ward.logan.t@gmail.com>
2023-08-09 19:39:19 -0400

    Update the convergence studies

--- cp2k/mt/1_run-tests.py
+++ cp2k/mt/1_run-tests.py
@@ -87,8 +87,8 @@ if __name__ == "__main__":
     )
     rmtree('run', ignore_errors=True)
     calc = CP2K(cutoff=args.cutoff * units.Ry, max_scf=10, directory='run',
-                command='/home/lward/Software/cp2k-2022.2/exe/local_cuda/cp2k_shell.ssmp' if args.gpu else  \
-                        '/home/lward/Software/cp2k-2022.2/exe/local/cp2k_shell.ssmp',
+                command='/home/lward/Software/cp2k-2023.2/exe/local_cuda/cp2k_shell.ssmp' if args.gpu else  \
+                        '/home/lward/Software/cp2k-2023.2/exe/local/cp2k_shell.ssmp',
                 **cp2k_opts)
 
     # Read in the example data
